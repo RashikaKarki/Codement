d39568e82d60815749df924b2d52e3d21ebb7b92
Shrill Shrestha <shrill.shrestha@gmail.com>
2021-03-26 18:46:39 -0500

    add upload file feat

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const fs = require('fs');
 const { ConnectDB } = require('./middlewares/db');
 
 require('dotenv').config();
@@ -9,12 +10,41 @@ const PORT = process.env.PORT || 8000;
 const MongoURL = process.env.MONGOURL;
 const app = express();
 
+if(!fs.existsSync('files')){
+  fs.mkdirSync('files');
+}
+
 try {
   ConnectDB(MongoURL);
 } catch (next) {}
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) =>{
+    cb(null, 'files');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.uname + "_" + file.originalname);
+  }
+});
+
+app.use(multer({storage: fileStorage}).single('source'))
+
+/**testing in browser*/
+// const path = require('path');
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (req, res) =>{
+//   res.render('index.html');
+// });
+
+// app.post("/file", (req, res, next) => {
+//   const fileInfo = req.file;
+//   console.log(fileInfo);
+// });
+/** */
 
 app.use((err, req, res, next) => {
   console.log(err);
