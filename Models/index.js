1b26859dffa1dd709c3ae1a89f74e1c694284100
Shrill Shrestha <shrill.shrestha@gmail.com>
2021-03-26 19:00:24 -0500

    add single entry point for models

---
+++ Models/index.js
@@ -0,0 +1,9 @@
+const userModel = require('./userModel');
+const fileModel = require('./fileModel');
+const commentModel = require('./commentModel');
+
+module.exports = {
+  userModel,
+  fileModel,
+  commentModel
+}
